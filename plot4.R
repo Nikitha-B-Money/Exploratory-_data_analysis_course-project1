54e68636e1eae181ac9126451b2e15077561339b
Nikitha-B-Money <nikithab1998@gmail.com>
2020-05-10 00:32:19 +0530

    course project 1

---
+++ plot4.R
@@ -0,0 +1,21 @@
+dataFile <- "C:/Users/Nikky/Desktop/Data Science/household_power_consumption.txt"
+data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+mydata<-data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+globalactivepower <- as.numeric(mydata$Global_active_power)
+volt <- as.numeric(mydata$Voltage)
+globalreactivepower <-as.numeric(mydata$Global_reactive_power)
+submetering1<- as.numeric(mydata$Sub_metering_1)
+submetering2 <- as.numeric(mydata$Sub_metering_2)
+submetering3 <- as.numeric(mydata$Sub_metering_3)
+days <-strptime(paste(mydata$Date,mydata$Time,sep=" ") ,"%d/%m/%Y %H:%M:%S")
+png("plot4.png",height=480,width=480)
+par(mfrow=c(2,2))
+plot(days , globalactivepower , type="l",xlab="",ylab="Global Active Power")
+plot(days ,volt,type="l",xlab = "datetime",ylab="Voltage")
+plot(days,submetering1,type="l",col="darkblue",xlab="",ylab="Energy sub metering") 
+lines(days,submetering2,type="l",col="red")
+lines(days,submetering3,type="l",col="blue")
+legend("topright",legend=c("sub_metering_1","sub_metering_2","sub_metering_3"),lty=1,lwd=2,col=c("darkblue","red","blue"),bty="o")
+plot(days,globalreactivepower,xlab="datetime",ylab="Global_reactive_power",type="l")
+dev.off()
